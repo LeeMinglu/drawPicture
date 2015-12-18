c6871aff11d4d26e820d12fb6015dd8dc99b94e8
LeeMinglu <402007353@qq.com>
2015-12-18 13:41:28 +0800

    项目初始化

---
+++ DrawPicture/DrawPicture/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DrawPicture
+//
+//  Created by 李明禄 on 15/12/18.
+//  Copyright © 2015年 SocererGroup. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DrawPicture/DrawPicture/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DrawPicture
+//
+//  Created by 李明禄 on 15/12/18.
+//  Copyright © 2015年 SocererGroup. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
